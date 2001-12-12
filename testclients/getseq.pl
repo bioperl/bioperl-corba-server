35c53880a9e6dfad3d24c4a8d8cc3f41d4697743
jason <jason@bioperl.org>
2001-12-12 19:49:38 +0000

    test for weird ORBit bug
    svn path=/bioperl-corba-server/trunk/; revision=126

--- testclients/getseq.pl
+++ testclients/getseq.pl
@@ -1,17 +1,37 @@
-#!/usr/local/bin/perl
-
-use CORBA::ORBit idl => [ '../idl/seqcore.idl' ];
-
-$ior_file = "simpleseq.ior";
+#!/usr/bin/perl -w
+use strict;
+use CORBA::ORBit idl => [ 'idl/seqcore.idl' ];
+use Error qw(:try);
+my $ior_file = "simpleseq.ior";
 print STDERR "Got file $ior_file\n";
-$orb = CORBA::ORB_init("orbit-local-orb");
+my $orb = CORBA::ORB_init("orbit-local-orb");
 
 open(F,"$ior_file") || die "Could not open $ior_file";
-$ior = <F>;
+my $ior = <F>;
 chomp $ior;
 close(F);
 
-$seq = $orb->string_to_object($ior);
-print "seq is $seq, ",ref($seq), "\n";
-print "sequence 1,10 is ",$seq->sub_seq(1,10),"\n";
+my $seq = $orb->string_to_object($ior);
+try {
+    print "seq is $seq, ",ref($seq), "\n";
+    print "sequence all is ", $seq->seq(), "\n";
+    print "length is ", $seq->get_length(), "\n";
+    print "sequence is ", ( ! $seq->is_circular() ) ? 'not ' : '', 
+    "circular\n";
+    
+    # WHAT THE HELL IS GOING ON!
+    # when $end 7->10 fail on RH7.0 CORBA 0.5.7  
+    # please test
+    for my $end ( 3..12) {
+	eval {
+	    print STDERR "sequence 1,$end is ",$seq->sub_seq(1,$end),"\n";
+	};
+    }    
+} catch bsane::OutOfBounds with {
+    my $E = shift;
+    print "exception ", $E->{'reason'}, "\n";
+} catch bsane::RequestTooLarge with {
+    my $E = shift;
+    print "exception: reason=", $E->{'reason'}, "\n";
 
+}
