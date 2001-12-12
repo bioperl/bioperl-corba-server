7a77e27417b29815cbe1e373e98f79746de7a073
jason <jason@bioperl.org>
2001-12-12 22:15:16 +0000

    updated for new interface
    svn path=/bioperl-corba-server/trunk/; revision=130

--- servers/genbankserver.pl
+++ servers/genbankserver.pl
@@ -7,17 +7,18 @@ use Bio::CorbaServer::Server;
 use Bio::Index::GenBank;
 my $tst_index_file = "/tmp/gbmam.dbm";
 my $ind = Bio::Index::GenBank->new($tst_index_file, 
-				 'WRITE');
+				   'WRITE');
 $ind->make_index("/tmp/gbmam.seq");
 # got to make sure the entire file is synced to disk before proceeeding...
-$ind = undef;
 
-my $seqdbindex = Bio::Index::GenBank->new($tst_index_file);
-
-my $server = new Bio::CorbaServer::Server ( -idl => 'biocorba.idl',
+my $server = new Bio::CorbaServer::Server ( -idl => 'idl/biocorba.idl',
 					    -ior => 'seqdbsrv.ior',
 					    -orbname=> 'orbit-local-orb');
 my $seqdb = $server->new_object(-object=>'Bio::CorbaServer::SeqDB',
-				-args => [ 'GenBank Mamalian', $seqdbindex ]);
+				-args => [ '-seqdb', $ind ]);
+
+print "starting server\n";
 
 $server->start();
+
+print "ready to go\n";
--- servers/simpleseq.pl
+++ servers/simpleseq.pl
@@ -1,6 +1,6 @@
 #!/usr/local/bin/perl -w
 
-use CORBA::ORBit idl => [ '../idl/seqcore.idl' ];
+use CORBA::ORBit idl => [ 'idl/biocorba.idl' ];
 use Bio::CorbaServer::PrimarySeq;
 use Bio::SeqIO;
 use Bio::PrimarySeq;
