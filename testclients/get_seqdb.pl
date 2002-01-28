7be6cdd4f65b784820b9617718c2fdd7ad8ce681
jason <jason@bioperl.org>
2002-01-28 18:15:29 +0000

    handle OUT parameters correctly
    svn path=/bioperl-corba-server/trunk/; revision=141

--- testclients/get_seqdb.pl
+++ testclients/get_seqdb.pl
@@ -1,13 +1,13 @@
 #!/usr/local/bin/perl
-
+use strict;
 use CORBA::ORBit idl => [ 'idl/biocorba.idl' ];
 use Error qw(:try);
-$ior_file = "seqdbsrv.ior";
+my $ior_file = "seqdbsrv.ior";
 print STDERR "Got file $ior_file\n";
-$orb = CORBA::ORB_init("orbit-local-orb");
+my $orb = CORBA::ORB_init("orbit-local-orb");
 
 open(F,"$ior_file") || die "Could not open $ior_file";
-$ior = <F>;
+my $ior = <F>;
 chomp $ior;
 close(F);
 
@@ -21,13 +21,20 @@ try {
 } catch bsane::IdentifierDoesNotExist with { 
     my $E = shift;
     print "Caught an exception for a nonexistent id - good!\n $E", 
-          $E->{'reason'}, "\n";    
+          $E->{'reason'}, "\n";
 };
 
 try {
     $seq = $db->resolve('HSEARLOBE');
     print "Seq is ", $seq->seq(), "\n";
-} except { 
+    my ($seqs,$iter) = $db->get_seqs(1);
+    print "iter is $iter seqs are $seqs\n";
+    my ($status,$seq);
+    while( (($status, $seq) = $iter->next()) && $status ) {
+	print "seq is $seq\n";
+	print "seq is ", $seq->get_name(), "\n";
+    }
+} catch bsane::OutOfBounds with { 
     my $E = shift;
     print "exception is $E", $E->{'reason'}, "\n";
 };
