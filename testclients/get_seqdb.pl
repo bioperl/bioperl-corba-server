17f79fdbd4cd1138d87d54d52241b9afd51ea9ba
jason <jason@bioperl.org>
2002-01-28 20:40:43 +0000

    test next_n and num_features_on_region
    svn path=/bioperl-corba-server/trunk/; revision=146

--- testclients/get_seqdb.pl
+++ testclients/get_seqdb.pl
@@ -1,6 +1,9 @@
 #!/usr/local/bin/perl
 use strict;
+use Data::Dumper;
 use CORBA::ORBit idl => [ 'idl/biocorba.idl' ];
+use Bio::CorbaServer::Utils qw(create_BSANE_location_from_Bioperl_location);
+
 use Error qw(:try);
 my $ior_file = "seqdbsrv.ior";
 print STDERR "Got file $ior_file\n";
@@ -33,8 +36,26 @@ try {
     while( (($status, $seq) = $iter->next()) && $status ) {
 	print "seq is $seq\n";
 	print "seq is ", $seq->get_name(), "\n";
+	my $sfc = $seq->get_seq_features();
+	my $loc = &create_BSANE_location_from_Bioperl_location(new Bio::Location::Simple('-start' => 1, '-end' => $seq->get_length-1, '-strand' => 1 ) );
+	#print "loc is ", Dumper($loc),"\n";
+	print "feature count is ", $sfc->num_features_on_region($loc),"\n";
     }
+    my ($seqs,$iter) = $db->get_seqs(1);
+    print "iter is $iter seqs are $seqs\n";
+
+    while( (($status, $seq) = $iter->next_n(2)) && $status ) {
+	print "status is $status\n";
+	foreach my $s ( @$seq ) {
+	    print "seq is $s\n";
+	}
+    }
+    
+
 } catch bsane::OutOfBounds with { 
+    my $E = shift;
+    print "exception is $E", $E->{'reason'}, "\n";
+} catch bsane::seqcore::SeqFeatureLocationOutOfBounds with {
     my $E = shift;
     print "exception is $E", $E->{'reason'}, "\n";
 };
